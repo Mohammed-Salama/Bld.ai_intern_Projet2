083215a4e98efc8afbebbea1a34432307929fae7
Mohammed-Salama <mohammedsalama1617@gmail.com>
2022-09-04 17:26:05 +0200

    fixing bug

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import HomePageSecondContainer from './HomePageSecondContainer.js'
 import WelcomeTextAndAlert from './WelcomeTextAndAlert.js'
-import {Link} from 'react-router-dom'
 function HomePage() {
   return (
     <>
         <WelcomeTextAndAlert />
-        <Link to="/courses/50"><button>lol </button></Link>
         <HomePageSecondContainer/>
     </>
   )
