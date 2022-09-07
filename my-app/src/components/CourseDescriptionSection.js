05d360c69c8d4c4da662f2c482d95cc354404319
Mohammed-Salama <mohammedsalama1617@gmail.com>
2022-09-07 03:46:34 +0200

    Show less and more done

---
+++ my-app/src/components/CourseDescriptionSection.js
@@ -0,0 +1,22 @@
+import React , {useCallback}  from 'react'
+import styles from './CourseDescriptionSectionStyles.module.css'
+import ViewHTML from './ViewHTML'
+import { Spoiler } from '@mantine/core';
+function CourseDescriptionSection(props) {
+
+    const  spoilerControlRef = useCallback(node => {
+        if (node !== null) {
+          node.className = styles.show_more_and_less_button;
+        }
+      }, []);
+  return (
+    <div  className={styles.description_section}>
+        <h2 className={styles.content_title}>Description</h2>
+        <Spoiler controlRef={spoilerControlRef} maxHeight={250} showLabel="Show more" hideLabel="Show less">
+            <ViewHTML data={props.data}/>
+        </Spoiler>
+    </div>
+  )
+}
+
+export default CourseDescriptionSection
--- my-app/src/components/CoursePageThirdContainer.js
+++ my-app/src/components/CoursePageThirdContainer.js
@@ -1,7 +1,9 @@
-import React , {useState} from 'react'
+import React , {useState ,useCallback} from 'react'
 import styles from './CoursePageThirdContainerStyles.module.css'
 import WhatYouWillLearn from './WhatYouWillLearn'
 import DropDown from './DropDown'
+import CourseDescriptionSection from './CourseDescriptionSection'
+import { Spoiler } from '@mantine/core';
 function CoursePageThirdContainer(props) {
   const [expand_collapse , setExpand_collapse] = useState("Expand");
   const [dropDowns , setDropDowns] = useState(props.data.content.map((dropDown,index) => 
@@ -28,20 +30,28 @@ function CoursePageThirdContainer(props) {
   )
 
 
+  const  spoilerControlRef = useCallback(node => {
+    if (node !== null) {
+      node.className = styles.show_more_and_less_button;
+    }
+  }, []);
   return (
     <div className={styles.course_page_third_container}>
-      <div id = "liop" className={styles.inner_container}>
+      <div  className={styles.inner_container}>
         <WhatYouWillLearn data={props.data} />
         <h2 className={styles.content_title}>Course content</h2>
         <div className={styles.info_and_expand}>
           <p className={styles.course_info}>{props.data.sections_num+" sections. "+props.data.lectures_num+" lecture. "+props.data.total_length+" total length."}</p>
           <button className={styles.expand_collapse_button} onClick={toggleExpandCollapse}>{expand_collapse + " all sections"}</button>
         </div>
-        {dropDowns}
+        <Spoiler controlRef={spoilerControlRef} maxHeight={260} showLabel="More sections" hideLabel="Less sections">
+            {dropDowns}
+        </Spoiler>
         <h2 className={styles.content_title}>Requirements</h2>
         <ul className={styles.requirements}>
           {requirements}
         </ul>
+        <CourseDescriptionSection data={props.data.description_section}/>
       </div>
     </div>
   )
