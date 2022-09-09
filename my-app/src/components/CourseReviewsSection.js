a30f86d857c2fcd945b55ee232268afca87274e6
Mohammed-Salama <mohammedsalama1617@gmail.com>
2022-09-09 02:39:14 +0200

    show more for reviews done

--- my-app/src/components/CourseReviewsSection.js
+++ my-app/src/components/CourseReviewsSection.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React , {useCallback} from 'react'
 import ReviewComponent from './ReviewComponent'
 import styles from './CourseReviewsSectionStyles.module.css'
+import { Spoiler } from '@mantine/core';
 function CourseReviewsSection(props) {
     const reviews = props.data.map(review => (
         <ReviewComponent key={review.id} data={review}/>
     ));
-
+    const  spoilerControlRef = useCallback(node => {
+      if (node !== null) {
+        node.className = styles.show_more_and_less_button;
+      }
+    }, []);
   return (
     <>
         <h2 className={styles.reviews_title}>Reviews</h2>
-        {reviews}
+
+        <Spoiler controlRef={spoilerControlRef} maxHeight={485} showLabel="More reviews" hideLabel="Less reviews">
+            {reviews}
+        </Spoiler>
     </>
   )
 }
