98188b2de254c98822d1a1d4910104ba4ed18fc1
Mohammed-Salama <mohammedsalama1617@gmail.com>
2022-09-10 03:36:34 +0200

    pop up done, finally the project is done Al hamdullela

--- my-app/src/components/CardsSection.js
+++ my-app/src/components/CardsSection.js
@@ -2,24 +2,25 @@ import React from 'react'
 import Card from './Card'
 import styles from './CardsSectionStyle.module.css'
 import {CoursesDataContext} from '../App.js';
-import {Link} from 'react-router-dom';
+
+
 function CardsSection() {
   const data = React.useContext(CoursesDataContext).courses_data;
   const searchParameters = React.useContext(CoursesDataContext).courses_filter;
   const courses = data.courses;
+
   const filteredCourses = courses.filter(course => (!searchParameters.get('courses_filter')) || course.title.toLowerCase().includes(searchParameters.get('courses_filter').toLowerCase()));
   return (
-
+    
     <div className={styles.courses_container}>
         <h1 className={styles.courses_container_inner_title}>{data.title}</h1>
         <p className={styles.courses_container_inner_description}>{data.description}</p>
         <button className={styles.explore_button}>{"Explore "+data.topic}</button>
         <div className={styles.courses_cards_box}>
             {filteredCourses.map(course => (
-              <Link key={course.id}  style={{ color: 'inherit', textDecoration: 'inherit'}} to = {`/courses/${course.id}`}>
-                <Card data={course}/>
-              </Link>
+              <Card key={course.id} data={course}/>
             ))}
+            
         </div>
     </div>
   )
